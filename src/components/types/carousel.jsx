426ee400942af9ba4502182a886d7607eab0494a
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-06-27 00:14:51 -0400

    Modificar la importación de React para mantener la consistencia en el código.

--- src/components/types/carousel.jsx
+++ src/components/types/carousel.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import * as React from "react";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { IMAGES } from "../consts/Images";
+
+        
