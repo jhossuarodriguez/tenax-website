3a6969d5f3519439c5297c79acc83f49270c7006
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-06-26 21:04:50 -0400

    Eliminar el componente Carousel de Projects.astro y actualizar las rutas de las imágenes en IMAGES.ts

--- src/components/consts/Images.ts
+++ src/components/consts/Images.ts
@@ -7,31 +7,31 @@ export interface Image {
 
 export const IMAGES: Image[] = [
     {
-        image: "/src/assets/images/banner1.webp",
+        image: "images/banner1.webp",
         alt: "Proyecto 1",
         title: "Proyecto 1",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/src/assets/images/banner2.webp",
+        image: "images/banner2.webp",
         alt: "Proyecto 2",
         title: "Proyecto 2",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/src/assets/images/banner3.webp",
+        image: "images/banner3.webp",
         alt: "Proyecto 3",
         title: "Proyecto 3",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/src/assets/images/banner4.webp",
+        image: "images/banner4.webp",
         alt: "Proyecto 4",
         title: "Proyecto 4",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/src/assets/images/banner5.webp",
+        image: "images/banner5.webp",
         alt: "Proyecto 5",
         title: "Proyecto 5",
         location: "Santo Domingo, RD",
