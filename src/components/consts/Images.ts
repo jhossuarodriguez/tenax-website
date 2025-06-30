d8409ab64ef3a54d693d0bd6fd02fba127bd21cd
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-06-30 15:31:45 -0400

    Renombrar constante de imágenes y actualizar atributos de banner en el componente Images

--- src/components/consts/Images.ts
+++ src/components/consts/Images.ts
@@ -5,37 +5,58 @@ export interface Image {
     location: string;
 }
 
-export const IMAGES: Image[] = [
+export const BANNER_IMAGES: Image[] = [
     {
         image: "/images/banner1.webp",
+        alt: "Banner 1",
+        title: "Banner 1",
+        location: "Santo Domingo, RD",
+    },
+    {
+        image: "/images/banner2.webp",
+        alt: "Banner 2",
+        title: "Banner 2",
+        location: "Santo Domingo, RD",
+    },
+    {
+        image: "/images/banner3.webp",
+        alt: "Banner 3",
+        title: "Banner 3",
+        location: "Santo Domingo, RD",
+    },
+    {
+        image: "/images/banner4.webp",
+        alt: "Banner 4",
+        title: "Banner 4",
+        location: "Santo Domingo, RD",
+    },
+    {
+        image: "/images/banner5.webp",
+        alt: "Banner 5",
+        title: "Banner 5",
+        location: "Santo Domingo, RD",
+    },
+] as const
+
+export const PROJECTS_IMAGES: Image[] = [
+    {
+        image: "/projects/1.webp",
         alt: "Proyecto 1",
         title: "Proyecto 1",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/images/banner2.webp",
+        image: "/projects/2.webp",
         alt: "Proyecto 2",
         title: "Proyecto 2",
         location: "Santo Domingo, RD",
     },
     {
-        image: "/images/banner3.webp",
+        image: "/projects/3.webp",
         alt: "Proyecto 3",
         title: "Proyecto 3",
         location: "Santo Domingo, RD",
     },
-    {
-        image: "/images/banner4.webp",
-        alt: "Proyecto 4",
-        title: "Proyecto 4",
-        location: "Santo Domingo, RD",
-    },
-    {
-        image: "/images/banner5.webp",
-        alt: "Proyecto 5",
-        title: "Proyecto 5",
-        location: "Santo Domingo, RD",
-    },
-] as const
+] as const;
 
 
