7a1bc9f3b64245651c894c11213bce6922c881cb
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-07-08 15:10:52 -0400

    Eliminar importaciones no utilizadas en el componente Carousel

--- src/components/types/carousel.jsx
+++ src/components/types/carousel.jsx
@@ -1,6 +1,1 @@
-import * as React from "react";
-import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
-import { IMAGES } from "../consts/Images";
 
-        
