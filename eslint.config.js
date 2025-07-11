3364a24b2f32caa829f7b87c5c3e4544530a0917
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-07-11 19:08:44 -0400

    Agregar configuración de ESLint con reglas recomendadas y ajustes de formato

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslint from 'eslint-plugin-astro'
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default {
+    // add more generic rule sets here, such as:
+    // js.configs.recommended,
+    ...eslintPluginAstro.configs.recommended,
+    rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+}
