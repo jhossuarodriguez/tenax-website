8b3d8114b0211032fb79e6c0b38de2a4512d4b3b
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2026-01-16 21:12:42 -0400

    fix: agregar integración de sitemap en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
+import path from 'node:path'
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import vercel from '@astrojs/vercel'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-
+  site: 'https://tenaxconstruction.com.do',
   output: 'server',
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
 
   build: {
@@ -21,7 +28,5 @@ export default defineConfig({
     responsiveStyles: true,
   },
 
-  integrations: [react()],
-
-  site: 'https://tenaxconstruction.com.do',
+  integrations: [react(), sitemap()],
 })
